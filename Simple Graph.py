c7ad96ca7e9f408872f7efd369b6dbedfdddffcd
DenisMario <120132656+DenisMario@users.noreply.github.com>
2022-12-09 12:43:16 -0700

    Add files via upload
    Simple graph with Labels

--- Simple Graph.py
+++ Simple Graph.py
@@ -1,4 +1,7 @@
 import matplotlib
 import matplotlib.pyplot as pt
 pt.plot([1,2,4,6], [8,12,16,32])
+pt.title("Amount of snow in December")
+pt.xlabel('Days in December')
+pt.ylabel('Inches of snow')
 pt.show()
